aa375aa4031f2d878a80ec7feec97d1cfdffaa1b
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2024-12-21 14:26:04 -0600

    v1.0.0

--- VoidCritVineBoom/Main.cs
+++ VoidCritVineBoom/Main.cs
@@ -10,10 +10,15 @@ using R2API;
 using R2API.Networking;
 using System.IO;
 using EntityStates.AffixEarthHealer;
+using RoR2.Audio;
+using System.Reflection;
+using System.Collections;
+using R2API.Networking.Interfaces;
 
 namespace SylmarDev.VoidCritVineBoom
 {
     [BepInDependency("com.bepis.r2api.sound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.bepis.r2api.prefab", BepInDependency.DependencyFlags.HardDependency)]
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     public class Main : BaseUnityPlugin
     {
@@ -22,33 +27,82 @@ namespace SylmarDev.VoidCritVineBoom
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginVersion = "1.0.0";
 
+        public bool shouldMuteOriginalSFX;
+
         public void Awake()
         {
             Log.Init(Logger);
             Log.Info($"{PluginGUID} // ver {PluginVersion}");
             Log.Info("Starting Process. . .");
 
+            new VoidCritVineBoomConfig().Init(Paths.ConfigPath);
+            shouldMuteOriginalSFX = VoidCritVineBoomConfig.muteOriginalVoidExecute.Value;
+
             NetworkingAPI.RegisterMessageType<SyncSound>();
-            Log.Debug("Registered Syncsound");
 
-            // TODO: Return to this later
-
-            using (Stream manifestResourceStream = base.GetType().Assembly.GetManifestResourceStream("vineboom.bnk"))
+            using (Stream manifestResourceStream = base.GetType().Assembly.GetManifestResourceStream("VoidCritVineBoom.Resources.vineboom.bnk"))
             {
                 byte[] array = new byte[manifestResourceStream.Length];
                 manifestResourceStream.Read(array, 0, array.Length);
                 SoundAPI.SoundBanks.Add(array);
             }
+            
+            On.RoR2.EffectManager.SpawnEffect_GameObject_EffectData_bool += EffectManager_SpawnEffect_GameObject_EffectData_bool;
 
-            On.RoR2.HealthComponent.TakeDamageProcess += HealthComponent_TakeDamageProcess;
-
+            if (shouldMuteOriginalSFX)
+            {
+                On.RoR2.EffectCatalog.GetEffectDef += EffectCatalog_GetEffectDef;
+            }
 
             Log.Info("awake complete");
         }
 
-        private void HealthComponent_TakeDamageProcess(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, HealthComponent self, DamageInfo damageInfo)
+        private IEnumerator DelayPostSound(float delayTime, Vector3 pos)
         {
-            
+            yield return new WaitForSeconds(delayTime);
+            GameObject obj = new GameObject
+            {
+                transform =
+                {
+                    position = pos
+                }
+            };
+
+            AkSoundEngine.PostEvent("Play_vineboom", obj);
+
+            NetMessageExtensions.Send(new SyncSound(pos), (NetworkDestination) 1);
+            yield break;
+        }
+
+        private void EffectManager_SpawnEffect_GameObject_EffectData_bool(On.RoR2.EffectManager.orig_SpawnEffect_GameObject_EffectData_bool orig, GameObject effectPrefab, EffectData effectData, bool transmit)
+        {
+            if (effectPrefab.name.Trim() == "CritGlassesVoidExecuteEffect")
+            {                
+                if (shouldMuteOriginalSFX)
+                {
+                    effectData.networkSoundEventIndex = NetworkSoundEventIndex.Invalid;
+                }
+
+                // play vine boom instead
+                StartCoroutine(DelayPostSound(0f, effectData.origin));
+            }
+
+            orig(effectPrefab, effectData, transmit);
+        }
+
+        private EffectDef EffectCatalog_GetEffectDef(On.RoR2.EffectCatalog.orig_GetEffectDef orig, EffectIndex effectIndex)
+        {
+            if (effectIndex != (EffectIndex) 136) // 136 is the id of void crit execute effect
+            {
+                return orig(effectIndex);
+            }
+
+            var value = orig(effectIndex);
+
+            var prefab = PrefabAPI.InstantiateClone(value.prefab, "mutedvoidexecute", false);
+            prefab.GetComponent<EffectComponent>().soundName = "";
+
+            return new EffectDef(prefab);
         }
 
         public void Update()
---
+++ VoidCritVineBoom/VoidCritVineBoomConfig.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SylmarDev.VoidCritVineBoom
+{
+    public class VoidCritVineBoomConfig
+    {
+        public static ConfigEntry<bool> muteOriginalVoidExecute;
+
+        public void Init(string configPath)
+        {
+            var config = new ConfigFile(Path.Combine(configPath, Main.PluginGUID + ".cfg"), true);
+
+            muteOriginalVoidExecute = config.Bind("Tweaks", "Mute Original Void Crit SFX", true, "Set to true to mute the vanilla Void Crit proc SFX");
+        }
+    }
+}
