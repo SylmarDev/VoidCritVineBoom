f00bcac638fac8277e283129435b5f796d4ac86e
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2024-12-20 21:17:33 -0600

    Mostly boilerplate, but have the sound bank now

---
+++ VoidCritVineBoom/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BepInEx;
+using R2API.Utils;
+using IL.RoR2.WwiseUtils;
+using On.RoR2.WwiseUtils;
+using RoR2.WwiseUtils;
+using UnityEngine;
+using RoR2;
+using R2API;
+using R2API.Networking;
+using System.IO;
+using EntityStates.AffixEarthHealer;
+
+namespace SylmarDev.VoidCritVineBoom
+{
+    [BepInDependency("com.bepis.r2api.sound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginAuthor = "SylmarDev";
+        public const string PluginName = "VoidCritVineBoom";
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginVersion = "1.0.0";
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+            Log.Info($"{PluginGUID} // ver {PluginVersion}");
+            Log.Info("Starting Process. . .");
+
+            NetworkingAPI.RegisterMessageType<SyncSound>();
+            Log.Debug("Registered Syncsound");
+
+            // TODO: Return to this later
+
+            using (Stream manifestResourceStream = base.GetType().Assembly.GetManifestResourceStream("vineboom.bnk"))
+            {
+                byte[] array = new byte[manifestResourceStream.Length];
+                manifestResourceStream.Read(array, 0, array.Length);
+                SoundAPI.SoundBanks.Add(array);
+            }
+
+            On.RoR2.HealthComponent.TakeDamageProcess += HealthComponent_TakeDamageProcess;
+
+
+            Log.Info("awake complete");
+        }
+
+        private void HealthComponent_TakeDamageProcess(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ VoidCritVineBoom/SyncSound.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using R2API.Networking.Interfaces;
+using UnityEngine.Networking;
+using UnityEngine;
+
+namespace SylmarDev.VoidCritVineBoom
+{
+    public class SyncSound : INetMessage, ISerializableObject
+    {
+        public SyncSound() { }
+
+        public SyncSound(UnityEngine.Vector3 soundPosition)
+        {
+            this.soundPosition = soundPosition;
+        }
+        public void Deserialize(NetworkReader reader)
+        {
+            this.soundPosition = reader.ReadVector3();
+        }
+        public void OnReceived()
+        {
+            if (!NetworkServer.active)
+            {
+                AkSoundEngine.PostEvent("Play_vineboom", new GameObject
+                {
+                    transform =
+                    {
+                        position = this.soundPosition
+                    }
+                });
+            }
+        }
+
+        public void Serialize(NetworkWriter writer)
+        {
+            writer.Write(this.soundPosition);
+        }
+
+        private UnityEngine.Vector3 soundPosition;
+    }
+}
